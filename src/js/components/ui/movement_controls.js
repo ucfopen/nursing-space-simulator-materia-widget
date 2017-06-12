ea37aa273eb0be23215e44463b7da592158f994d
Corey Peterson <corey.peterson@ucf.edu>
2017-06-12 15:01:20 -0400

    WIP new UI icons

--- src/js/components/ui/movement_controls.js
+++ src/js/components/ui/movement_controls.js
@@ -15,15 +15,15 @@ export default class MovementControls extends React.Component {
 			<div id="UI-right-panel">
 				<div id="top-buttons" />
 				<button onClick={this.takeScreenShot.bind(this)} id="screenshot">
-					Take a Screenshot
+					<img src="assets/icon-svgs/screenshot.svg" width="60px" height="60px" alt="Take a Screenshot" />
 				</button>
 				<div id="camera-move">
-					<button id="camera-up" onClick={this.props.xUp}>&uarr;</button>
+					<button id="camera-up" onClick={this.props.xUp}><img src="assets/icon-svgs/up_arrow.svg" width="50px" height="50px" alt="Move camera or asset up" /></button>
 					<div id="camera-move-horizontal">
-						<button id="camera-left" onClick={this.props.zUp}>&larr;</button>
-						<button id="camera-right" onClick={this.props.zDown}>&rarr;</button>
+						<button id="camera-left" onClick={this.props.zUp}><img src="assets/icon-svgs/left_arrow.svg" width="50px" height="50px" alt="Move camera or asset left" /></button>
+						<button id="camera-right" onClick={this.props.zDown}><img src="assets/icon-svgs/right_arrow.svg" width="50px" height="50px" alt="Move camera or asset right" /></button>
 					</div>
-					<button id="camera-down" onClick={this.props.xDown}>&darr;</button>
+					<button id="camera-down" onClick={this.props.xDown}><img src="assets/icon-svgs/down_arrow.svg" width="50px" height="50px" alt="Move camera or asset down" /></button>
 				</div>
 				<button id="camera-zoom-in" onClick={this.props.yDown}>+</button>
 				<button id="camera-zoom-out" onClick={this.props.yUp}>-</button>
