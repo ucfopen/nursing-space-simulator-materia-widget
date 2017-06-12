58bd2aca47d70ca15ffba79cdd3dd4996ae7be63
Ryan Eppers <ryan.eppers@ucf.edu>
2017-06-12 14:15:45 -0400

    formatted js files using prettier

--- src/js/components/ui/asset_button.js
+++ src/js/components/ui/asset_button.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export default class AssetButton extends React.Component {
-    render () {
-        return (
-            <button 
-                className="asset" 
-                data-category={this.props.item.category}
-                data-title={this.props.item.title}
-                id={this.props.item.id}
-                onClick={this.props.onClick}
-                style={{background: 'url(' + this.props.item.buttonSource + ') no-repeat center center', backgroundSize: "100% 100%"}}/>
-        )
-    }
+	render() {
+		return (
+			<button
+				className="asset"
+				data-category={this.props.item.category}
+				data-title={this.props.item.title}
+				id={this.props.item.id}
+				onClick={this.props.onClick}
+				style={{
+					background:
+						"url(" + this.props.item.buttonSource + ") no-repeat center center",
+					backgroundSize: "100% 100%"
+				}}
+			/>
+		);
+	}
 }
