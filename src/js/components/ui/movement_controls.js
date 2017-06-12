8d2a8ccf3971da51467823a306654190dc1ef6b1
Ryan Eppers <ryan.eppers@ucf.edu>
2017-06-12 14:15:45 -0400

    formatted js files using prettier

--- src/js/components/ui/movement_controls.js
+++ src/js/components/ui/movement_controls.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 export default class MovementControls extends React.Component {
-    // TODO: GET THIS OUT ASAP, this is only in for the demo on the week of 6/12/17
-    takeScreenShot() {
-        document.querySelector('a-scene').components.screenshot.capture('perspective');
-    }
-    // BAD ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	// TODO: GET THIS OUT ASAP, this is only in for the demo on the week of 6/12/17
+	takeScreenShot() {
+		document
+			.querySelector("a-scene")
+			.components.screenshot.capture("perspective");
+	}
+	// BAD ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-    render () {
-        return (
-            <div id="UI-right-panel">
-                <div id="top-buttons"></div>
-                <button onClick={this.takeScreenShot.bind(this)} id="screenshot">Take a Screenshot</button>
-                <div id="camera-move">
-                    <button id="camera-up" onClick={this.props.xUp}>&uarr;</button>
-                    <div id="camera-move-horizontal">
-                        <button id="camera-left" onClick={this.props.zUp}>&larr;</button>
-                        <button id="camera-right" onClick={this.props.zDown} >&rarr;</button>
-                    </div>
-                    <button id="camera-down" onClick={this.props.xDown}>&darr;</button>
-                </div>
-                <button id="camera-zoom-in" onClick={this.props.yDown}>+</button>
-                <button id="camera-zoom-out" onClick={this.props.yUp}>-</button>
-                <button id="camera-position-reset" onClick={this.props.resetPosition}>Reset</button>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div id="UI-right-panel">
+				<div id="top-buttons" />
+				<button onClick={this.takeScreenShot.bind(this)} id="screenshot">
+					Take a Screenshot
+				</button>
+				<div id="camera-move">
+					<button id="camera-up" onClick={this.props.xUp}>&uarr;</button>
+					<div id="camera-move-horizontal">
+						<button id="camera-left" onClick={this.props.zUp}>&larr;</button>
+						<button id="camera-right" onClick={this.props.zDown}>&rarr;</button>
+					</div>
+					<button id="camera-down" onClick={this.props.xDown}>&darr;</button>
+				</div>
+				<button id="camera-zoom-in" onClick={this.props.yDown}>+</button>
+				<button id="camera-zoom-out" onClick={this.props.yUp}>-</button>
+				<button id="camera-position-reset" onClick={this.props.resetPosition}>
+					Reset
+				</button>
+			</div>
+		);
+	}
 }
