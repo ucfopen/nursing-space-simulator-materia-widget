86e237340819dd9c0693117bec625c05490a6f09
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2017-06-14 16:26:29 -0400

    Screenshot button now shows up in both camera modes

--- src/js/components/hud.js
+++ src/js/components/hud.js
@@ -10,35 +10,41 @@ import MovementControls from "./ui/movement_controls";
 
 export default class HUD extends React.Component {
 	render() {
-		return this.props.thirdPerson
-			? <div>
-					<MovementControls
-						xUp={this.props.xUp.bind(this)}
-						xDown={this.props.xDown.bind(this)}
-						yUp={this.props.yUp.bind(this)}
-						yDown={this.props.yDown.bind(this)}
-						zUp={this.props.zUp.bind(this)}
-						zDown={this.props.zDown.bind(this)}
-						resetPosition={this.props.resetPosition.bind(this)}
-					/>
-					{this.props.selectedAsset !== null
-						? <AssetControls
-								manipulateAsset={this.props.manipulateAsset}
+		return (
+			<div>
+				<MovementControls
+					thirdPerson={this.props.thirdPerson}
+					xUp={this.props.xUp.bind(this)}
+					xDown={this.props.xDown.bind(this)}
+					yUp={this.props.yUp.bind(this)}
+					yDown={this.props.yDown.bind(this)}
+					zUp={this.props.zUp.bind(this)}
+					zDown={this.props.zDown.bind(this)}
+					resetPosition={this.props.resetPosition.bind(this)}
+				/>
+				{this.props.thirdPerson
+					? <div>
+							{this.props.selectedAsset !== null
+								? <AssetControls
+										manipulateAsset={this.props.manipulateAsset}
+										selectedAsset={this.props.selectedAsset}
+										manipulationMode={this.props.manipulationMode}
+									/>
+								: null}
+							<AssetTray
+								assetsFromFile={this.props.assetsFromFile}
+								categories={this.props.categories}
+								selectAsset={this.props.selectAsset.bind(this)}
 								selectedAsset={this.props.selectedAsset}
-								manipulationMode={this.props.manipulationMode}
 							/>
-						: null}
-					<AssetTray
-						assetsFromFile={this.props.assetsFromFile}
-						categories={this.props.categories}
-						selectAsset={this.props.selectAsset.bind(this)}
-						selectedAsset={this.props.selectedAsset}
-					/>
-				</div>
-			: <div id="ground-top-panel">
-					<button id="back" onClick={this.props.toggleCamera.bind(this)}>
-						Back
-					</button>
-				</div>;
+						</div>
+					: <div id="ground-top-panel">
+							<button id="back" onClick={this.props.toggleCamera.bind(this)}>
+								Back
+							</button>
+
+						</div>}
+			</div>
+		);
 	}
 }
