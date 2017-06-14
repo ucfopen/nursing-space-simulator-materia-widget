74757c17309337ea87052981935aa26b4b950d4f
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2017-06-14 12:47:32 -0400

    Crrently selected asssets still show up when non-existing assets are selected and controls only show up for assets existing on the grid

--- src/js/components/ui/asset_controls.js
+++ src/js/components/ui/asset_controls.js
@@ -6,47 +6,51 @@ import DeselectSVG from "../assets/icon-svgs/deselect";
 
 export default class AssetControls extends React.Component {
 	render() {
-		return this.props.manipulationMode
-			? <div id="UI-selected-asset-options">
-					<span id="selected-asset-label">
-						<span className="selected-asset-label-title">
-							Currently selected: {this.props.selectedAsset.asset.title}
-						</span>
+		return (
+			<div id="UI-selected-asset-options">
+				<span id="selected-asset-label">
+					<span className="selected-asset-label-title">
+						Currently selected: {this.props.selectedAsset.asset.title}
 					</span>
-					<button
-						id="deselect"
-						onClick={this.props.manipulateAsset.bind(
-							this,
-							this.props.selectedAsset.asset,
-							"deselect",
-							this.props.selectedAsset.x,
-							this.props.selectedAsset.y
-						)}>
-						<DeselectSVG />
-					</button>
-					<button
-						id="rotate"
-						onClick={this.props.manipulateAsset.bind(
-							this,
-							this.props.selectedAsset.asset,
-							"rotate",
-							this.props.selectedAsset.x,
-							this.props.selectedAsset.y
-						)}>
-						<RotateSVG />
-					</button>
-					<button
-						id="remove"
-						onClick={this.props.manipulateAsset.bind(
-							this,
-							this.props.selectedAsset.asset,
-							"remove",
-							this.props.selectedAsset.x,
-							this.props.selectedAsset.y
-						)}>
-						<DeleteSVG />
-					</button>
-				</div>
-			: null;
+				</span>
+				{this.props.manipulationMode
+					? <div>
+							<button
+								id="deselect"
+								onClick={this.props.manipulateAsset.bind(
+									this,
+									this.props.selectedAsset.asset,
+									"deselect",
+									this.props.selectedAsset.x,
+									this.props.selectedAsset.y
+								)}>
+								<DeselectSVG />
+							</button>
+							<button
+								id="rotate"
+								onClick={this.props.manipulateAsset.bind(
+									this,
+									this.props.selectedAsset.asset,
+									"rotate",
+									this.props.selectedAsset.x,
+									this.props.selectedAsset.y
+								)}>
+								<RotateSVG />
+							</button>
+							<button
+								id="remove"
+								onClick={this.props.manipulateAsset.bind(
+									this,
+									this.props.selectedAsset.asset,
+									"remove",
+									this.props.selectedAsset.x,
+									this.props.selectedAsset.y
+								)}>
+								<DeleteSVG />
+							</button>
+						</div>
+					: null}
+			</div>
+		);
 	}
 }
