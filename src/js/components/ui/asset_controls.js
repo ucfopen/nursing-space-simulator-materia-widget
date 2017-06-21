6ab7db4122417326b91a76aee8eab4f44415f733
Ryan Eppers <ryan.eppers@ucf.edu>
2017-06-21 12:58:59 -0400

    converted ui components to functional components

--- src/js/components/ui/asset_controls.js
+++ src/js/components/ui/asset_controls.js
@@ -5,63 +5,61 @@ import DeselectSVG from "../assets/icon-svgs/deselect";
 import RotateSVG from "../assets/icon-svgs/rotate";
 import ConfirmSVG from "../assets/icon-svgs/confirm";
 
-export default class AssetControls extends React.Component {
-	render() {
-		return (
-			<div id="UI-selected-asset-options">
-				<span id="selected-asset-label">
-					<span className="selected-asset-label-title">
-						Currently selected: {this.props.selectedAsset.asset.title}
-					</span>
+export default props => {
+	return (
+		<div id="UI-selected-asset-options">
+			<span id="selected-asset-label">
+				<span className="selected-asset-label-title">
+					Currently selected: {props.selectedAsset.asset.title}
 				</span>
-				{this.props.manipulationMode
-					? <div>
-							<button
-								id="confirm"
-								onClick={this.props.manipulateAsset.bind(
-									this,
-									this.props.selectedAsset.asset,
-									"deselect",
-									this.props.selectedAsset.x,
-									this.props.selectedAsset.y
-								)}>
-								<ConfirmSVG />
-							</button>
-							<button
-								id="rotate"
-								onClick={this.props.manipulateAsset.bind(
-									this,
-									this.props.selectedAsset.asset,
-									"rotate",
-									this.props.selectedAsset.x,
-									this.props.selectedAsset.y
-								)}>
-								<RotateSVG />
-							</button>
-							<button
-								id="remove"
-								onClick={this.props.manipulateAsset.bind(
-									this,
-									this.props.selectedAsset.asset,
-									"remove",
-									this.props.selectedAsset.x,
-									this.props.selectedAsset.y
-								)}>
-								<DeleteSVG />
-							</button>
-						</div>
-					: <button
-							id="deselect"
-							onClick={this.props.manipulateAsset.bind(
+			</span>
+			{props.manipulationMode
+				? <div>
+						<button
+							id="confirm"
+							onClick={props.manipulateAsset.bind(
 								this,
-								this.props.selectedAsset.asset,
+								props.selectedAsset.asset,
 								"deselect",
-								this.props.selectedAsset.x,
-								this.props.selectedAsset.y
+								props.selectedAsset.x,
+								props.selectedAsset.y
 							)}>
-							<DeselectSVG />
-						</button>}
-			</div>
-		);
-	}
-}
+							<ConfirmSVG />
+						</button>
+						<button
+							id="rotate"
+							onClick={props.manipulateAsset.bind(
+								this,
+								props.selectedAsset.asset,
+								"rotate",
+								props.selectedAsset.x,
+								props.selectedAsset.y
+							)}>
+							<RotateSVG />
+						</button>
+						<button
+							id="remove"
+							onClick={props.manipulateAsset.bind(
+								this,
+								props.selectedAsset.asset,
+								"remove",
+								props.selectedAsset.x,
+								props.selectedAsset.y
+							)}>
+							<DeleteSVG />
+						</button>
+					</div>
+				: <button
+						id="deselect"
+						onClick={props.manipulateAsset.bind(
+							this,
+							props.selectedAsset.asset,
+							"deselect",
+							props.selectedAsset.x,
+							props.selectedAsset.y
+						)}>
+						<DeselectSVG />
+					</button>}
+		</div>
+	);
+};
