1d4cf6432018bc5053aa4abeb3e7f0734c54fb79
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2017-06-21 13:36:09 -0400

    Separated out grid logic into own file

--- src/js/reducers/placementReducer.js
+++ src/js/reducers/placementReducer.js
@@ -1,112 +1,113 @@
-import {
-  SELECT_ASSET_TYPE,
-  SELECT_OBJECT,
-  DESELECT_OBJECT,
-  ROTATE_OBJECT,
-  REMOVE_OBJECT,
-  UPDATE_GRID
-} from "../actions/placement_actions"
+import { rotateCell, resetCell, insertItem } from "../grid";
 
-import { INIT_DATA } from "../actions"
+import {
+	SELECT_ASSET_TYPE,
+	SELECT_OBJECT,
+	DESELECT_OBJECT,
+	ROTATE_OBJECT,
+	REMOVE_OBJECT,
+	UPDATE_GRID
+} from "../actions/placement_actions";
+
+import { INIT_DATA } from "../actions";
 
 export default function(
-  state = { manipulationMode: false, selectedAsset: "none" },
-  action
+	state = { manipulationMode: false, selectedAsset: "none" },
+	action
 ) {
-  switch (action.type) {
-    case INIT_DATA:
-      return { ...state, grid: action.payload.grid }
-    case SELECT_ASSET_TYPE:
-      return {
-        ...state,
-        selectedAsset: action.payload,
-        manipulationMode: false,
-        currentX: null,
-        currentY: null
-      }
-    case SELECT_OBJECT:
-      let oldSelectedAsset
-      if (state.selectedAsset === "none") oldSelectedAsset = "none"
-      else oldSelectedAsset = { ...state.selectedAsset }
+	switch (action.type) {
+		case INIT_DATA:
+			return { ...state, grid: action.payload.grid };
+		case SELECT_ASSET_TYPE:
+			return {
+				...state,
+				selectedAsset: action.payload,
+				manipulationMode: false,
+				currentX: null,
+				currentY: null
+			};
+		case SELECT_OBJECT:
+			let oldSelectedAsset;
+			if (state.selectedAsset === "none") oldSelectedAsset = "none";
+			else oldSelectedAsset = { ...state.selectedAsset };
 
-      if (
-        oldSelectedAsset.id !== action.payload.asset.id &&
-        oldSelectedAsset !== "none"
-      ) {
-        let gridWithReplacedObject = JSON.parse(JSON.stringify(state.grid))
-        gridWithReplacedObject[action.payload.x][action.payload.y] = {
-          id: oldSelectedAsset.id,
-          rotation: 0
-        }
-        return {
-          ...state,
-          manipulationMode: true,
-          selectedAsset: oldSelectedAsset,
-          currentX: action.payload.x,
-          currentY: action.payload.y,
-          grid: gridWithReplacedObject
-        }
-      } else {
-        let gridWithReplacedObject = JSON.parse(JSON.stringify(state.grid))
-        gridWithReplacedObject[action.payload.x][action.payload.y] = {
-          id: action.payload.asset.id,
-          rotation: 0
-        }
-        return {
-          ...state,
-          manipulationMode: true,
-          selectedAsset: action.payload.asset,
-          currentX: action.payload.x,
-          currentY: action.payload.y,
-          grid: gridWithReplacedObject
-        }
-      }
-    case DESELECT_OBJECT:
-      return {
-        ...state,
-        manipulationMode: false,
-        selectedAsset: "none",
-        currentX: null,
-        currentY: null
-      }
-    case ROTATE_OBJECT: {
-      let gridWithRotation = JSON.parse(JSON.stringify(state.grid))
-      const currentRotation =
-        gridWithRotation[action.payload.x][action.payload.y].rotation
-      gridWithRotation[action.payload.x][action.payload.y].rotation =
-        (currentRotation + 90) % 360
-      return { ...state, grid: gridWithRotation }
-    }
-    case REMOVE_OBJECT: {
-      let gridWithObjectRemoved = JSON.parse(JSON.stringify(state.grid))
-      gridWithObjectRemoved[action.payload.x][action.payload.y] = "0"
-      return {
-        ...state,
-        grid: gridWithObjectRemoved,
-        manipulationMode: false,
-        selectedAsset: "none",
-        currentX: null,
-        currentY: null
-      }
-    }
-    case UPDATE_GRID:
-      if (state.selectedAsset === "none")
-        return {
-          ...state
-        }
-      let updatedGrid = JSON.parse(JSON.stringify(state.grid))
-      updatedGrid[action.payload.x][action.payload.y] = {
-        id: state.selectedAsset.id,
-        rotation: 0
-      }
-      return {
-        ...state,
-        grid: updatedGrid,
-        manipulationMode: true,
-        currentX: action.payload.x,
-        currentY: action.payload.y
-      }
-    default:
-      return state
-  }
+			if (
+				oldSelectedAsset.id !== action.payload.asset.id &&
+				oldSelectedAsset !== "none"
+			) {
+				let gridWithReplacedObject = JSON.parse(JSON.stringify(state.grid));
+				gridWithReplacedObject[action.payload.x][action.payload.y] = {
+					id: oldSelectedAsset.id,
+					rotation: 0
+				};
+				return {
+					...state,
+					manipulationMode: true,
+					selectedAsset: oldSelectedAsset,
+					currentX: action.payload.x,
+					currentY: action.payload.y,
+					grid: gridWithReplacedObject
+				};
+			} else {
+				let gridWithReplacedObject = JSON.parse(JSON.stringify(state.grid));
+				gridWithReplacedObject[action.payload.x][action.payload.y] = {
+					id: action.payload.asset.id,
+					rotation: 0
+				};
+				return {
+					...state,
+					manipulationMode: true,
+					selectedAsset: action.payload.asset,
+					currentX: action.payload.x,
+					currentY: action.payload.y,
+					grid: gridWithReplacedObject
+				};
+			}
+		case DESELECT_OBJECT:
+			return {
+				...state,
+				manipulationMode: false,
+				selectedAsset: "none",
+				currentX: null,
+				currentY: null
+			};
+		case ROTATE_OBJECT: {
+			let gridCopy = JSON.parse(JSON.stringify(state.grid));
+			return {
+				...state,
+				grid: rotateCell(gridCopy, action.payload.x, action.payload.y)
+			};
+		}
+		case REMOVE_OBJECT: {
+			let gridCopy = JSON.parse(JSON.stringify(state.grid));
+			return {
+				...state,
+				grid: deleteItem(gridCopy, action.payload.x, action.payload.y),
+				manipulationMode: false,
+				selectedAsset: "none",
+				currentX: null,
+				currentY: null
+			};
+		}
+		case UPDATE_GRID:
+			if (state.selectedAsset === "none")
+				return {
+					...state
+				};
+			let gridCopy = JSON.parse(JSON.stringify(state.grid));
+			return {
+				...state,
+				grid: insertItem(
+					gridCopy,
+					state.selectedAsset.id,
+					action.payload.x,
+					action.payload.y
+				),
+				manipulationMode: true,
+				currentX: action.payload.x,
+				currentY: action.payload.y
+			};
+		default:
+			return state;
+	}
 }
