cb9085ccbc5e782d2be720f4ab494f0a31823c8f
Ryan Eppers <ryan.eppers@ucf.edu>
2017-06-21 12:58:59 -0400

    converted ui components to functional components

--- src/js/components/assets/icon-svgs/deselect.js
+++ src/js/components/assets/icon-svgs/deselect.js
@@ -1,30 +1,25 @@
 import React from "react";
 
-export default class DeselectSVG extends React.Component {
-	render() {
-		return (
-			<svg
-				version="1.1"
-				xmlns="http://www.w3.org/2000/svg"
-				x="0px"
-				y="0px"
-				viewBox="0 0 125 125"
-				enable-background="new 0 0 125 125"
-				width="55px"
-				height="55px">
-				<g id="Layer_8">
-					<path
-						fill="#DC7627"
-						stroke="#000000"
-						strokeWidth="3"
-						stroke-miterlimit="10"
-						d="M62.5,7.2C31.9,7.2,7.2,31.9,7.2,62.5
-						s24.8,55.3,55.3,55.3s55.3-24.8,55.3-55.3S93.1,7.2,62.5,7.2z M62.5,26.7c5.2,0,10.1,1.1,14.6,3.1L29.8,77.1
-						c-2-4.5-3.1-9.4-3.1-14.6C26.7,42.7,42.7,26.7,62.5,26.7z M62.5,98.3c-5.2,0-10.1-1.1-14.6-3.1l47.3-47.3c2,4.5,3.1,9.4,3.1,14.6
-						C98.3,82.3,82.3,98.3,62.5,98.3z"
-					/>
-				</g>
-			</svg>
-		);
-	}
-}
+export default () => {
+	return (
+		<svg
+			version="1.1"
+			xmlns="http://www.w3.org/2000/svg"
+			x="0px"
+			y="0px"
+			viewBox="0 0 125 125"
+			enable-background="new 0 0 125 125"
+			width="55px"
+			height="55px">
+			<g id="Layer_8">
+				<path
+					fill="#DC7627"
+					stroke="#000000"
+					strokeWidth="3"
+					stroke-miterlimit="10"
+					d="M62.5,7.2C31.9,7.2,7.2,31.9,7.2,62.5s24.8,55.3,55.3,55.3s55.3-24.8,55.3-55.3S93.1,7.2,62.5,7.2z M62.5,26.7c5.2,0,10.1,1.1,14.6,3.1L29.8,77.1c-2-4.5-3.1-9.4-3.1-14.6C26.7,42.7,42.7,26.7,62.5,26.7z M62.5,98.3c-5.2,0-10.1-1.1-14.6-3.1l47.3-47.3c2,4.5,3.1,9.4,3.1,14.6C98.3,82.3,82.3,98.3,62.5,98.3z"
+				/>
+			</g>
+		</svg>
+	);
+};
--- src/js/components/assets/icon-svgs/rotate.js
+++ src/js/components/assets/icon-svgs/rotate.js
@@ -1,28 +1,24 @@
 import React from "react";
 
-export default class RotateSVG extends React.Component {
-	render() {
-		return (
-			<svg
-				version="1.1"
-				xmlns="http://www.w3.org/2000/svg"
-				x="0px"
-				y="0px"
-				viewBox="0 0 125 125"
-				width="55px"
-				height="55px">
-				<g id="Layer_2">
-					<path
-						fill="#DC7627"
-						stroke="#000000"
-						strokeWidth="3"
-						stroke-miterlimit="10"
-						d="M69.8,10.4v21.3c16.8,0.4,30.3,14.1,30.3,31
-						c0,17.1-13.9,31-31,31c-16.6,0-30.2-13.1-31-29.6h13.1L27.5,32.9L3.8,64.2h13.2c0.9,28,23.8,50.4,52,50.4
-						c28.8,0,52.1-23.3,52.1-52.1C121.2,34,98.2,10.8,69.8,10.4z"
-					/>
-				</g>
-			</svg>
-		);
-	}
-}
+export default () => {
+	return (
+		<svg
+			version="1.1"
+			xmlns="http://www.w3.org/2000/svg"
+			x="0px"
+			y="0px"
+			viewBox="0 0 125 125"
+			width="55px"
+			height="55px">
+			<g id="Layer_2">
+				<path
+					fill="#DC7627"
+					stroke="#000000"
+					strokeWidth="3"
+					stroke-miterlimit="10"
+					d="M69.8,10.4v21.3c16.8,0.4,30.3,14.1,30.3,31c0,17.1-13.9,31-31,31c-16.6,0-30.2-13.1-31-29.6h13.1L27.5,32.9L3.8,64.2h13.2c0.9,28,23.8,50.4,52,50.4c28.8,0,52.1-23.3,52.1-52.1C121.2,34,98.2,10.8,69.8,10.4z"
+				/>
+			</g>
+		</svg>
+	);
+};
