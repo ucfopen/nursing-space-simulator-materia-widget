7dea1ebedb540eab6376fc8962fcf1d255ea3abd
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2017-06-21 13:36:09 -0400

    Separated out grid logic into own file

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,34 +1,19 @@
-export const INIT_DATA = "init_data"
+import { loadGrid } from "../grid";
 
-function getAssetInfo(gridValue) {
-  if (gridValue === "0") return gridValue
-  const assetInfo = gridValue.split(".")
-  return {
-    id: assetInfo[0],
-    rotation: parseInt(assetInfo[1])
-  }
-}
-
-function loadGrid(gridString, rows, columns) {
-  let tempGrid = gridString.split(" ")
-  return [...Array(rows)].map(() => {
-    return tempGrid.splice(0, columns).map(gridItem => {
-      return getAssetInfo(gridItem)
-    })
-  })
-}
+export const INIT_DATA = "init_data";
 
 export function initData(qset) {
-  const grid = loadGrid(
-    qset.options.gridLoader.content,
-    qset.options.gridLoader.rows,
-    qset.options.gridLoader.columns
-  )
-  const categories = qset.options.categories
-  const assets = qset.options.assets
+	const grid = loadGrid(
+		qset.options.gridLoader.content,
+		qset.options.gridLoader.rows,
+		qset.options.gridLoader.columns
+	);
 
-  return {
-    type: INIT_DATA,
-    payload: { grid, categories, assets }
-  }
+	const categories = qset.options.categories;
+	const assets = qset.options.assets;
+
+	return {
+		type: INIT_DATA,
+		payload: { grid, categories, assets }
+	};
 }
