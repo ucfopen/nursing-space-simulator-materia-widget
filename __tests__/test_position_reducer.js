e013c4699d2ba698bce3a84572560ab48fd72f92
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2017-06-26 17:05:56 -0400

    Added tests for actions, reducers, and some snapshot testing

---
+++ __tests__/test_position_reducer.js
@@ -0,0 +1,184 @@
+import positionReducer from "../src/js/reducers/positionReducer";
+import * as actions from "../src/js/actions/camera_actions";
+
+describe("position reducer", () => {
+	it("should return the inital state", () => {
+		expect(
+			positionReducer(
+				// State being passed in
+				undefined,
+				// Action being passed in
+				{}
+			)
+		).toEqual({
+			x: 2.5,
+			y: 18,
+			z: 14,
+			thirdPerson: true
+		});
+	});
+
+	it("should handle POSITION_UPDATE", () => {
+		const x = 10,
+			y = 10,
+			z = 10;
+
+		let direction = "xUp";
+
+		expect(
+			positionReducer(
+				// State being passed in
+				{ x, y, z },
+				// Action being passed in
+				{
+					type: actions.POSITION_UPDATE,
+					payload: direction
+				}
+			)
+		).toEqual({
+			x: x + 1,
+			y,
+			z
+		});
+
+		direction = "xDown";
+
+		expect(
+			positionReducer(
+				// State being passed in
+				{ x, y, z },
+				// Action being passed in
+				{
+					type: actions.POSITION_UPDATE,
+					payload: direction
+				}
+			)
+		).toEqual({
+			x: x - 1,
+			y,
+			z
+		});
+
+		direction = "yUp";
+
+		expect(
+			positionReducer(
+				// State being passed in
+				{ x, y, z },
+				// Action being passed in
+				{
+					type: actions.POSITION_UPDATE,
+					payload: direction
+				}
+			)
+		).toEqual({
+			x,
+			y: y + 1,
+			z
+		});
+
+		direction = "yDown";
+
+		expect(
+			positionReducer(
+				// State being passed in
+				{ x, y, z },
+				// Action being passed in
+				{
+					type: actions.POSITION_UPDATE,
+					payload: direction
+				}
+			)
+		).toEqual({
+			x,
+			y: y - 1,
+			z
+		});
+
+		direction = "zUp";
+
+		expect(
+			positionReducer(
+				// State being passed in
+				{ x, y, z },
+				// Action being passed in
+				{
+					type: actions.POSITION_UPDATE,
+					payload: direction
+				}
+			)
+		).toEqual({
+			x,
+			y,
+			z: z - 1
+		});
+
+		direction = "zDown";
+
+		expect(
+			positionReducer(
+				// State being passed in
+				{ x, y, z },
+				// Action being passed in
+				{
+					type: actions.POSITION_UPDATE,
+					payload: direction
+				}
+			)
+		).toEqual({
+			x,
+			y,
+			z: z + 1
+		});
+
+		direction = "reset";
+
+		expect(
+			positionReducer(
+				// State being passed in
+				{ x, y, z },
+				// Action being passed in
+				{
+					type: actions.POSITION_UPDATE,
+					payload: direction
+				}
+			)
+		).toEqual({
+			x,
+			y: 2,
+			z
+		});
+	});
+
+	it("should handle CAMERA_TOGGLE", () => {
+		let thirdPerson = true;
+
+		expect(
+			positionReducer(
+				// State being passed in
+				{ thirdPerson },
+				// Action being passed in
+				{
+					type: actions.CAMERA_TOGGLE
+				}
+			)
+		).toEqual({
+			thirdPerson: false
+		});
+
+		thirdPerson = false;
+
+		expect(
+			positionReducer(
+				// State being passed in
+				{ thirdPerson },
+				// Action being passed in
+				{
+					type: actions.CAMERA_TOGGLE
+				}
+			)
+		).toEqual({
+			thirdPerson: true
+		});
+	});
+});
