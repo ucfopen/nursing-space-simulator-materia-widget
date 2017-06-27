4ca6c9df99e3ef41e3fbcd8c91bdd20582ae3cfe
Ryan Eppers <ryan.eppers@ucf.edu>
2017-06-27 14:36:27 -0400

    first steps to getting tour integrated with redux setup

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -3,12 +3,14 @@ import Data from "./dataReducer";
 import Position from "./positionReducer";
 import Menu from "./menuReducer";
 import Grid from "./gridReducer";
+import Tour from "./tourReducer";
 
 const rootReducer = combineReducers({
 	data: Data,
 	position: Position,
 	menu: Menu,
-	grid: Grid
+	grid: Grid,
+	tour: Tour
 });
 
 export default rootReducer;
