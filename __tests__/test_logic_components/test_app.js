1fdf0ad7916e8659fcd23b7ab73ef220e19660f1
Ryan Eppers <ryan.eppers@ucf.edu>
2017-06-29 17:29:55 -0400

    app test for componentDidMount working

--- __tests__/test_logic_components/test_app.js
+++ __tests__/test_logic_components/test_app.js
@@ -4,35 +4,13 @@ import { Provider } from "react-redux";
 import qset from "../custom_test_utilities/mock_qset";
 import { mount, shallow } from "enzyme";
 import { createStore } from "redux";
-import renderer from "react-test-renderer";
 import reducers from "../../src/js/reducers";
 import { JSDOM } from "jsdom";
-import Aframe from "aframe";
-import Sinon from "sinon";
-
-const propsInApp = ["store", "qset", "initData"];
-/* setup.js */
 
 const jsdom = new JSDOM(
 	"<!doctype html><html><body><div id='app'></div></body></html>"
 );
-const { window } = jsdom;
 
-function copyProps(src, target) {
-	const props = Object.getOwnPropertyNames(src)
-		.filter(prop => typeof target[prop] === "undefined")
-		.map(prop => Object.getOwnPropertyDescriptor(src, prop));
-	Object.defineProperties(target, props);
-}
-
-global.window = window;
-global.document = window.document;
-global.navigator = {
-	userAgent: "node.js"
-};
-global.AFRAME = Aframe;
-
-copyProps(window, global);
 describe("App tests", () => {
 	it("should. ...", () => {
 		const app = shallow(<App qset={qset} />);
@@ -40,14 +18,16 @@ describe("App tests", () => {
 
 	it("should render connected component using app", () => {
 		const store = createStore(reducers);
-
+		const spy = jest.fn();
+		ConnectedApp.prototype.componentDidMount = function() {
+			spy();
+		};
 		const connectedAppComponent = mount(
-			<Provider store={store}><ConnectedApp qset={qset} /></Provider>,
+			<Provider store={store}>
+				<ConnectedApp qset={qset} />
+			</Provider>,
 			{ attachTo: jsdom.window.document.getElementById("app") }
 		);
-
-		console.log(
-			connectedAppComponent.find(ConnectedApp).find(App).find("a-entity")
-		);
+		expect(spy).toBeCalled();
 	});
 });
