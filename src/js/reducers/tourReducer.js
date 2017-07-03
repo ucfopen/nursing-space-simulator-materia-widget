c3a2094167750097dfce2d232a0e3959c50e34f0
Ryan Eppers <ryan.eppers@ucf.edu>
2017-07-03 16:26:22 -0400

    tour functional

--- src/js/reducers/tourReducer.js
+++ src/js/reducers/tourReducer.js
@@ -1,40 +1,74 @@
 import { START_TOUR_SECTION } from "../actions/tour_actions";
-import { SELECT_ASSET_TYPE } from "../actions/grid_actions";
+import { SELECT_ASSET_TYPE, INSERT_ASSET } from "../actions/grid_actions";
 import { INIT_DATA } from "../actions/index";
 
 export default function(
 	state = {
+		tourRunning: true,
 		steps: [],
-		vrSceneClicked: false,
-		vrSceneHaveEnteredFirstPerson: false,
-		currentStepSet: 0,
-		runNextSet: false
+		nextSteps: [],
+		runNextSet: false,
+		stepSetInQueue: "part1",
+		stepCompletion: { 1: false, 2: false, 3: false, 4: false, 5: false }
 	},
 	action
 ) {
 	switch (action.type) {
 		case INIT_DATA:
-			return { ...state, runNextSet: true };
+			return {
+				...state,
+				runNextSet: true
+			};
+
 		case START_TOUR_SECTION:
 			return {
 				...state,
 				steps: action.payload.steps,
-				currentStepSet: (state.currentStepSet += 1),
+				nextSteps: action.payload.nextSteps,
+				stepSetInQueue: action.payload.stepSetInQueue,
+				stepCompletion: action.payload.stepCompletion,
 				runNextSet: false
 			};
+
 		case SELECT_ASSET_TYPE:
 			if (
-				state.vrSceneClicked === false &&
+				state.tourRunning &&
+				action.payload.id !== "pov_camera" &&
+				JSON.stringify(state.stepCompletion) ===
+					JSON.stringify({ 1: true, 2: false, 3: false, 4: false, 5: false })
+			) {
+				return {
+					...state,
+					runNextSet: true,
+					stepSetInQueue: "clickInScene"
+				};
+			} else if (
+				state.tourRunning &&
+				action.payload.id === "pov_camera" &&
+				JSON.stringify(state.stepCompletion) ===
+					JSON.stringify({ 1: true, 2: true, 3: true, 4: false, 5: false })
+			) {
+				return {
+					...state,
+					runNextSet: true,
+					stepSetInQueue: "clickFirstPersonViewer"
+				};
+			} else return state;
+
+		case INSERT_ASSET:
+			if (
+				state.tourRunning &&
+				JSON.stringify(state.stepCompletion) ===
+					JSON.stringify({ 1: true, 2: true, 3: false, 4: false, 5: false }) &&
 				action.payload.id !== "pov_camera"
 			) {
 				return {
 					...state,
-					vrSceneClicked: true,
-					steps: action.payload.steps,
 					runNextSet: true,
-					currentStepSet: (state.currentStepSet += 1)
+					stepSetInQueue: "part2"
 				};
 			} else return state;
+
 		default:
 			return state;
 	}
