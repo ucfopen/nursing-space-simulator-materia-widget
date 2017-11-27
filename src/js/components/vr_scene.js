b921b6922554173565d361f8575a08fd74a0332c
Nathan Dabu <nathan.dabu@ucf.edu>
2017-11-27 14:31:58 -0500

    color of hovered tile is red/green/white depending on situation rather than only red/white

--- src/js/components/vr_scene.js
+++ src/js/components/vr_scene.js
@@ -78,6 +78,7 @@ export class VRScene extends Component {
 							z={rowIndex}
 							onClick={insertAsset}
 							key={`${rowIndex} ${colIndex}`}
+							grid={this.props.grid}
 						/>
 					),
 					this
