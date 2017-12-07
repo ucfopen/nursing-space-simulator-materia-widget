9db48938d2493f546c01d9e2699f77801dd151a6
Nathan Dabu <nathan.dabu@ucf.edu>
2017-12-07 14:39:26 -0500

    moved to new wall type using simple boxes instead of 3d models -- much faster

--- src/js/components/assets/qset_asset.js
+++ src/js/components/assets/qset_asset.js
@@ -1,34 +1,44 @@
 import React, { Component } from "react";
 import AFRAME from "aframe";
 import { Entity } from "aframe-react";
+import Wall from "./wall";
 
 export default props => {
 	const yScaleFactor = props.data.category === "walls"
 		? props.data.scale.y / 2
 		: 0;
 	return (
-		<Entity
-			events={{ click: props.onClick }}
-			primitive={props.data.tag}
-			// If an assets contains an .mtl (defined in qset), aframe will use obj-model. Otherwise, aframe will use material src
-			material={{ color: "green", opacity: 0.4 }}
-			obj-model={
-				props.isSelected
-					? "obj: #" + props.data.id + "-obj;"
-					: "obj: #" +
-							props.data.id +
-							"-obj;" +
-							"mtl: #" +
-							props.data.id +
-							"-mtl;"
-			}
-			/** 
-			* The scale property grows an a-box in the +-y direction, but we want it to seem like the box is just growing in the +y direction.
-			*   To do this, the position of the box must be shifted upward half of the total scaling value.
-			*/
-			position={{ x: props.x, y: yScaleFactor, z: props.z }}
-			rotation={{ x: 0, y: props.rotation, z: 0 }}
-			scale={props.data.scale}
-		/>
+		(props.data.id === 'wall-1' || props.data.id === 'window') ?
+			<Wall
+				onClick={props.onClick}
+				isSelected={props.isSelected}
+				x={props.x}
+				z={props.z}
+				rotation={props.rotation}
+				type={props.data.id}
+			/> :
+			<Entity
+				events={{ click: props.onClick }}
+				primitive={props.data.tag}
+				// If an assets contains an .mtl (defined in qset), aframe will use obj-model. Otherwise, aframe will use material src
+				material={{ color: "green", opacity: 0.4}}
+				obj-model={
+					props.isSelected
+						? "obj: #" + props.data.id + "-obj;"
+						: "obj: #" +
+								props.data.id +
+								"-obj;" +
+								"mtl: #" +
+								props.data.id +
+								"-mtl;"
+				}
+				/**
+				* The scale property grows an a-box in the +-y direction, but we want it to seem like the box is just growing in the +y direction.
+				*   To do this, the position of the box must be shifted upward half of the total scaling value.
+				*/
+				position={{ x: props.x, y: yScaleFactor, z: props.z }}
+				rotation={{ x: 0, y: props.rotation, z: 0 }}
+				scale={props.data.scale}
+			/>
 	);
 };
