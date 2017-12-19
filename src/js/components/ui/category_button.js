ceb6460fe982aac60c1540e64a07ee3ae8fa8973
Ryan Eppers <ryan.eppers@ucf.edu>
2017-12-19 14:27:15 -0500

    removed beds category, uppercase letter for category name, removed underline from selection css, made equipment default category

--- src/js/components/ui/category_button.js
+++ src/js/components/ui/category_button.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 export default props => {
 	const { category, currentCategory, onClick } = props
+	const toUpper = word => word[0].toUpperCase() + word.substr(1)
 	return (
 		<button
 			className={category == currentCategory ? 'active-category asset-category' : 'asset-category'}
 			data-category={category}
 			onClick={onClick}
 		>
-			{category}
+			{toUpper(category)}
 		</button>
 	)
 }
