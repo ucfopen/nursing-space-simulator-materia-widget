f31d5605e18459d2b8ebb992a097f161b23f8c81
Ryan Eppers <ryan.eppers@ucf.edu>
2017-12-20 16:07:11 -0500

    added browser check that only enables drag and drop for chrome browser

--- src/js/components/assets/qset_asset.js
+++ src/js/components/assets/qset_asset.js
@@ -74,8 +74,52 @@ export default class QsetAsset extends Component {
 		);
 	}
 
+	renderAssetNonDrag() {
+		const { data, isSelected, x, z, rotation, attributes, mode } = this.props;
+		const { onClick } = this.props;
+		const yScaleFactor = data.category === "walls" ? data.scale.y / 2 : 0;
+		const id = data.id;
+
+		return id === "wall-1" || id === "window" || id == "door-1" ? (
+			<Wall
+				onClick={onClick}
+				isSelected={isSelected}
+				x={x}
+				z={z}
+				rotation={rotation}
+				type={data.id}
+				attributes={attributes}
+				mode={mode}
+			/>
+		) : (
+			<Entity
+				events={{
+					click: onClick
+				}}
+				primitive={data.tag}
+				// If an assets contains an .mtl (defined in qset), aframe will use obj-model. Otherwise, aframe will use material src
+				material={{ color: "green", opacity: 0.4 }}
+				obj-model={
+					isSelected
+						? `obj: #${data.id}-obj;`
+						: `obj: #${data.id}-obj;mtl: #${data.id}-mtl;`
+				}
+				/**
+				 * The scale property grows an a-box in the +-y direction, but we want it to seem like the box is just growing in the +y direction.
+				 *   To do this, the position of the box must be shifted upward half of the total scaling value.
+				 */
+				position={{ x, y: yScaleFactor, z }}
+				rotation={{ x: 0, y: rotation, z: 0 }}
+				scale={data.scale}
+			/>
+		);
+	}
+
 	render() {
-		if (checkPropsExist(this.props)) return this.renderAsset();
+		if (checkPropsExist(this.props))
+			return Namespace("HospitalSim").browserIsChrome
+				? this.renderAsset()
+				: this.renderAssetNonDrag();
 		else return null;
 	}
 }
