0f09117dc481657e40bf79e6bb9b68cd33da49e5
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2017-12-20 10:11:48 -0500

    Organized props and imports in ascending alphabetical order for custom aframe components

--- src/js/components/assets/camera_tp.js
+++ src/js/components/assets/camera_tp.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
 import { Entity } from "aframe-react";
 import "aframe-mouse-cursor-component";
+import React, { Component } from "react";
 
 export default props => {
 	return (
 		<Entity
 			camera={{ active: props.active }}
-			rotation="270 0 0"
-			position={props.position}
 			mouse-cursor
+			position={props.position}
+			rotation="270 0 0"
 		/>
 	);
 };
--- src/js/components/assets/ceiling.js
+++ src/js/components/assets/ceiling.js
@@ -3,16 +3,14 @@ import { Entity } from "aframe-react";
 import React from "react";
 
 export default props => {
-	return (
-		props.thirdPerson
-			? null
-			: <Entity
-				primitive="a-plane"
-				material="src: #ceilingTexture; repeat: 30 12; metalness: .15"
-				position={{ x: "14.5", y: "3", z: "5.5" }}
-				rotation={{ x: "90", y: "0", z: "0" }}
-				height="12"
-				width="30"
-			/>
+	return props.thirdPerson ? null : (
+		<Entity
+			height="12"
+			material="src: #ceilingTexture; repeat: 30 12; metalness: .15"
+			position={{ x: "14.5", y: "3", z: "5.5" }}
+			primitive="a-plane"
+			rotation={{ x: "90", y: "0", z: "0" }}
+			width="30"
+		/>
 	);
 };
