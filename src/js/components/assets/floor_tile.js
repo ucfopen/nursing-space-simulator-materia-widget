d4bc106d31b025a870c4aadf0a17f8d55ae6c4ba
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2017-12-20 15:14:15 -0500

    Resized floor tiles to be the same size as the highlighted gid block

--- src/js/components/assets/floor_tile.js
+++ src/js/components/assets/floor_tile.js
@@ -6,7 +6,7 @@ export default () => {
 	return (
 		<Entity
 			height="12"
-			material="src: #ceilingTexture; repeat: 30 12; metalness: .15"
+			material="src: #ceilingTexture; repeat: 10 4; metalness: .15"
 			position={{ x: "14.5", y: ".1", z: "5.5" }}
 			primitive="a-plane"
 			rotation={{ x: "-90", y: "0", z: "0" }}
