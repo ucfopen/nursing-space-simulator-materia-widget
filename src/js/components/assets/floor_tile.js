7efcf6cf123b51ba175a104297b924438461580d
Ryan Eppers <ryan.eppers@ucf.edu>
2017-12-20 13:05:05 -0500

    updated texture repeating pattern for floor

--- src/js/components/assets/floor_tile.js
+++ src/js/components/assets/floor_tile.js
@@ -6,7 +6,7 @@ export default () => {
 	return (
 		<Entity
 			primitive="a-plane"
-			material="src: #ceilingTexture; repeat: 30 12; metalness: .15"
+			material="src: #ceilingTexture; repeat: 10 4; metalness: .15"
 			position={{ x: "14.5", y: ".1", z: "5.5" }}
 			rotation={{ x: "-90", y: "0", z: "0" }}
 			height="12"
