d877d91b71d43eb06f521e81b938bb22cebaef5f
Nathan Dabu <nathan.dabu@ucf.edu>
2018-01-09 16:14:20 -0500

    added tooltips for mouseovers in manipulation mode

--- src/js/components/ui/asset_controls.js
+++ src/js/components/ui/asset_controls.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 // Custom React Components
@@ -25,104 +25,137 @@ const RIGHT = 1;
 const BOTTOM = 2;
 const LEFT = 3;
 
-export default props => {
-	// matches keys from props to respective variables
-	const { currentX, currentZ, grid, mode, selectedAsset, stickers } = props;
+export default class AssetControls extends Component {
 
-	const {
-		deselectAsset,
-		editAsset,
-		editSticker,
-		extendWall,
-		removeAsset,
-		rotateAsset
-	} = props;
-
-	if (mode === "extendWall")
-		return (
-			<div id="UI-selected-asset-options">
-				<div>
-					<AssetTooltip
-						showToolTip={true}
-						toolTipText="Click on a valid space to auto-fill walls."
-					/>
-					<button id="deselect" onClick={() => deselectAsset()}>
-						<DeselectSVG />
-					</button>
-				</div>
-			</div>
-		);
-	if (mode === "editAsset") {
-		const rotation = getCellRotation(grid, currentX, currentZ);
-		const stickers = getStickers(grid, currentX, currentZ, true);
-		const stickerProps = {
-			editSticker,
-			stickers,
-			rotation,
-			id: selectedAsset.id,
-			x: currentX,
-			z: currentZ
+	constructor(props) {
+		super(props);
+		this.state = {
+			showTooltip: false,
+			toolTipText: "abc"
 		};
+	}
 
-		return (
-			<div id="UI-selected-asset-options">
-				<div>
-					<AssetTooltip showToolTip={true} toolTipText="Edit this item." />
-					<button id="confirm" onClick={() => deselectAsset(true)}>
-						<ConfirmSVG />
-					</button>
-					<StickerBox index={TOP} side="top" {...stickerProps} />
-					<StickerBox index={RIGHT} side="right" {...stickerProps} />
-					<StickerBox index={BOTTOM} side="bottom" {...stickerProps} />
-					<StickerBox index={LEFT} side="left" {...stickerProps} />
-				</div>
-			</div>
-		);
-	} else // manipulationMode
-		return (
-			<div id="UI-selected-asset-options">
-				<span id="selected-asset-label">
-					<span className="selected-asset-label-title">
-						Currently selected: {selectedAsset.title}
-					</span>
-				</span>
-				{selectedAsset.id == "pov_camera" ? (
-					<AssetTooltip
-						showToolTip={true}
-						toolTipText="Select a location to jump into first-person view."
-					/>
-				) : null}
-				{mode == "manipulation" ? (
+	onMouseEnter(text) {
+		this.setState({ showTooltip: true, toolTipText: text });
+	}
+
+	onMouseLeave() {
+		this.setState({ showTooltip: false });
+	}
+
+	render() {
+		// matches keys from props to respective variables
+		const { currentX, currentZ, grid, mode, selectedAsset, stickers } = this.props;
+
+		const {
+			deselectAsset,
+			editAsset,
+			editSticker,
+			extendWall,
+			removeAsset,
+			rotateAsset
+		} = this.props;
+
+		if (mode === "extendWall")
+			return (
+				<div id="UI-selected-asset-options">
 					<div>
-						<button id="confirm" onClick={() => deselectAsset()}>
+						<AssetTooltip
+							showTooltip={true}
+							toolTipText="Click on a valid space to auto-fill walls."
+						/>
+						<button id="deselect" onClick={() => deselectAsset()}>
+							<DeselectSVG />
+						</button>
+					</div>
+				</div>
+			);
+		if (mode === "editAsset") {
+			const rotation = getCellRotation(grid, currentX, currentZ);
+			const stickers = getStickers(grid, currentX, currentZ, true);
+			const stickerProps = {
+				editSticker,
+				stickers,
+				rotation,
+				id: selectedAsset.id,
+				x: currentX,
+				z: currentZ
+			};
+
+			return (
+				<div id="UI-selected-asset-options">
+					<div>
+						<AssetTooltip showTooltip={true} toolTipText="Edit this item." />
+						<button id="confirm" onClick={() => deselectAsset(true)}>
 							<ConfirmSVG />
 						</button>
-						<button id="rotate" onClick={() => rotateAsset(currentX, currentZ)}>
-							<RotateSVG />
-						</button>
-						<button id="remove" onClick={() => removeAsset(currentX, currentZ)}>
-							<DeleteSVG />
-						</button>
-						{selectedAsset.id == "wall-1" ? (
-							<button
-								id="extendWall"
-								onClick={() => extendWall(currentX, currentZ)}>
-								<ExtendSVG />
-							</button>
-						) : null}
-						{selectedAsset.id == "wall-1" || selectedAsset.id == "door-1" ? (
-							<button
-								id="editAsset"
-								onClick={() => editAsset(currentX, currentZ)}>
-								<StickerSVG />
-							</button>
-						) : null}
+						<StickerBox index={TOP} side="top" {...stickerProps} />
+						<StickerBox index={RIGHT} side="right" {...stickerProps} />
+						<StickerBox index={BOTTOM} side="bottom" {...stickerProps} />
+						<StickerBox index={LEFT} side="left" {...stickerProps} />
 					</div>
-				) : (
-					<button id="deselect" onClick={() => deselectAsset()}>
-						<DeselectSVG />
-					</button>
-				)}
-			</div>
-		);
-};
+				</div>
+			);
+		} else // manipulationMode
+			return (
+				<div id="UI-selected-asset-options">
+					<span id="selected-asset-label">
+						<span className="selected-asset-label-title">
+							Currently selected: {selectedAsset.title}
+						</span>
+					</span>
+					<AssetTooltip
+						showTooltip={this.state.showTooltip}
+						toolTipText={this.state.toolTipText}
+					/>
+					{mode == "manipulation" ? (
+						<div>
+							<button
+								id="confirm"
+								onClick={() => deselectAsset()}
+								onMouseEnter={() => this.onMouseEnter("Confirm")}
+								onMouseLeave={() => this.onMouseLeave()}>
+								<ConfirmSVG/>
+							</button>
+							<button
+								id="rotate"
+								onClick={() => rotateAsset(currentX, currentZ)}
+								onMouseEnter={() => this.onMouseEnter("Rotate")}
+								onMouseLeave={() => this.onMouseLeave()}>
+								<RotateSVG />
+							</button>
+							<button
+								id="remove"
+								onClick={() => removeAsset(currentX, currentZ)}
+								onMouseEnter={() => this.onMouseEnter("Remove")}
+								onMouseLeave={() => this.onMouseLeave()}>
+								<DeleteSVG />
+							</button>
+							{selectedAsset.id == "wall-1" ? (
+								<button
+									id="extendWall"
+									onClick={() => extendWall(currentX, currentZ)}
+									onMouseEnter={() => this.onMouseEnter("Extend Wall")}
+									onMouseLeave={() => this.onMouseLeave()}>
+									<ExtendSVG />
+								</button>
+							) : null}
+							{selectedAsset.id == "wall-1" || selectedAsset.id == "door-1" ? (
+								<button
+									id="editAsset"
+									onClick={() => editAsset(currentX, currentZ)}
+									onMouseEnter={() => this.onMouseEnter("Attach Mode")}
+									onMouseLeave={() => this.onMouseLeave()}>
+									<StickerSVG />
+								</button>
+							) : null}
+						</div>
+					) : (
+						<button id="deselect" onClick={() => deselectAsset()}>
+							<DeselectSVG />
+						</button>
+					)}
+				</div>
+			);
+	}
+}
