600bf2d8124bf5187aa50cf6852c7878e718852e
Nathan Dabu <nathan.dabu@ucf.edu>
2018-02-13 15:49:03 -0500

    updated math for arrow placement

--- src/js/components/assets/arrow.js
+++ src/js/components/assets/arrow.js
@@ -8,19 +8,17 @@ import ArrowPrimitive from "./primitive_assets/arrow";
 export default class Arrow extends Component {
 
 	render() {
-		const { isSelected, mode, onClick, rotation, type, x, y, z } = this.props;
+		const { isSelected, mode, onClick, radius, rotation, type, x, z } = this.props;
 		const defaultProps = {
 			color: "#f9d9c2",
-			isSelected,
 			metalness: 0.4,
-			mode,
 			onClick,
+			radius,
 			rotation,
 			selectionColor: "green",
 			selectionOpacity: 0.4,
 			trimColor: "#7c695b",
 			x,
-			y,
 			z
 		};
 
