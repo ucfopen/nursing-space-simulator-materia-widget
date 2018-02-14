44249b54adb87ab2d5d486001d19baf0e2181b26
Nathan Dabu <nathan.dabu@ucf.edu>
2018-02-14 11:31:37 -0500

    added mouseover colors for arrows

--- src/js/components/assets/arrow.js
+++ src/js/components/assets/arrow.js
@@ -1,28 +1,22 @@
 import AFRAME from "aframe";
 import { Entity } from "aframe-react";
-import React, { Component } from "react";
+import React from "react";
 
 // Custom React Components
 import ArrowPrimitive from "./primitive_assets/arrow";
 
-export default class Arrow extends Component {
+export default props => {
+	const { onClick, radius, rotation, scale, x, z } = props;
+	const defaultProps = {
+		color: "#d08751",
+		onClick,
+		radius,
+		rotation,
+		scale,
+		selectionColor: "#e8a26d",
+		x,
+		z
+	};
 
-	render() {
-		const { onClick, radius, rotation, scale, x, z } = this.props;
-		const defaultProps = {
-			color: "#f9d9c2",
-			metalness: 0.4,
-			onClick,
-			radius,
-			rotation,
-			scale,
-			selectionColor: "green",
-			selectionOpacity: 0.4,
-			trimColor: "#7c695b",
-			x,
-			z
-		};
-
-		return <ArrowPrimitive {...defaultProps} />;
-	}
-}
+	return <ArrowPrimitive {...defaultProps} />;
+};
