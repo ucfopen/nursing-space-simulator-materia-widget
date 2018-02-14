4abd45897d464389288175a4129b8376b83c1d43
Nathan Dabu <nathan.dabu@ucf.edu>
2018-02-14 11:31:37 -0500

    added mouseover colors for arrows

--- src/js/components/assets/primitive_assets/arrow.js
+++ src/js/components/assets/primitive_assets/arrow.js
@@ -1,43 +1,67 @@
 import AFRAME from "aframe";
 import { Entity } from "aframe-react";
-import React from "react";
+import React, { Component } from "react";
 
-export default props => {
-	// attributes
-	const {
-		color,
-		metalness,
-		radius,
-		rotation,
-		scale,
-		selectionColor,
-		selectionOpacity,
-		trimColor,
-		x,
-		z
-	} = props;
+export default class ArrowPrimitive extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { active: false };
+	}
 
-	// functions
-	const { onClick } = props;
+	onMouseEnter() {
+		this.setState({ active: true });
+	}
 
-	return (
-		<Entity
-			events={{ click: onClick }}
-			position={{ x, y: 3.2, z }}
-			rotation={{ x: -90, y: rotation, z: 0 }}
-			scale={{ x:scale, y: scale, z: scale }}>
+	onMouseLeave() {
+		this.setState({ active: false });
+	}
+
+	render () {
+		// attributes
+		const {
+			color,
+			radius,
+			rotation,
+			scale,
+			selectionColor,
+			x,
+			z
+		} = this.props;
+
+		// functions
+		const { onClick } = this.props;
+
+		return (
 			<Entity
-				events={{ click: onClick }}
-				geometry={{ primitive: "plane", height: .8, width: .5 }}
-				material={{ color: "#e8a26d", opacity: 1, side: "double" }}
-				position={{ x: 0, y: radius + 1, z: 0 }}
-			/>
-			<Entity
-				geometry={{ primitive: "triangle", vertices: "0 0 0, 2 0 0, 1 1 0" }}
-				material={{ color: "#e8a26d", opacity: 1, side: "double" }}
-				position={{ x:-0.55, y: radius + 1.25, z: 0 }}
-				scale={{x: 0.55, y: 0.8}}
-			/>
-		</Entity>
-	);
-};
+				events={{
+					click: onClick,
+					mouseenter: this.onMouseEnter.bind(this),
+					mouseleave: this.onMouseLeave.bind(this)
+				}}
+				position={{ x, y: 3.2, z }}
+				rotation={{ x: -90, y: rotation, z: 0 }}
+				scale={{ x:scale, y: scale, z: scale }}>
+				<Entity
+					events={{ click: onClick }}
+					geometry={{ primitive: "plane", height: .8, width: .5 }}
+					material={
+						this.state.active
+							? { color: selectionColor, opacity: 1, side: "double" }
+							: { color: color, opacity: 1, side: "double" }
+					}
+					position={{ x: 0, y: radius + 1, z: 0 }}
+				/>
+				<Entity
+					geometry={{ primitive: "triangle", vertices: "0 0 0, 2 0 0, 1 1 0" }}
+					material={
+						this.state.active
+							? { color: selectionColor, opacity: 1, side: "double" }
+							: { color: color, opacity: 1, side: "double" }
+					}
+					position={{ x:-0.55, y: radius + 1.25, z: 0 }}
+					scale={{x: 0.55, y: 0.8}}
+				/>
+			</Entity>
+		);
+	}
+}
