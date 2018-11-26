e8419b5c54ca33114ce1e1e0b98f2a4e0c616aa0
Anthony Rodriguez <anthony.rodriguez@ucf.edu>
2018-11-26 17:32:43 -0500

    Added tests/improved coverage for tooltipReducer

--- __tests__/test_reducers/test_tooltip_reducer.js
+++ __tests__/test_reducers/test_tooltip_reducer.js
@@ -12,6 +12,8 @@ import {
 	UPDATE_TIMED_TOOLTIP
 } from "../../src/js/actions/tooltip_actions";
 
+import { EXTEND_WALL } from "../../src/js/actions/grid_actions";
+
 const initialState = {
 	className: null,
 	temporaryKey: null,
@@ -147,21 +149,68 @@ describe("tooltip reducer", () => {
 		});
 	});
 
-	it("should handle UPDATE_TIMED_TOOLTIP", () => {
+	it("should handle UPDATE_TIMED_TOOLTIP when state.temporaryKey == action.payload", () => {
 		expect(
-			menuReducer(initialState, {
-				type: UPDATE_TIMED_TOOLTIP,
-				payload: {
-					className: "test_name",
-					enabled: true,
-					text: "test_text",
-					key: "test_key"
+			menuReducer(
+				{
+					...initialState,
+					temporaryKey: "test_payload"
+				},
+				{
+					type: UPDATE_TIMED_TOOLTIP,
+					payload: "test_payload"
 				}
-			})
+			)
 		).toEqual({
 			className: null,
 			persistent: false,
 			persistentText: "persistent",
+			prevSelectedType: null,
+			temporary: false,
+			temporaryKey: null,
+			temporaryText: "temp"
+		});
+	});
+
+	it("should handle UPDATE_TIMED_TOOLTIP when state.temporaryKey != action.payload", () => {
+		expect(
+			menuReducer(
+				{
+					...initialState,
+					temporaryKey: "test_key"
+				},
+				{
+					type: UPDATE_TIMED_TOOLTIP,
+					payload: "test_payload"
+				}
+			)
+		).toEqual({
+			className: null,
+			persistent: false,
+			persistentText: "persistent",
+			prevSelectedType: null,
+			temporary: false,
+			temporaryKey: "test_key",
+			temporaryText: "temp"
+		});
+	});
+
+	it("should handle EXTEND_WALL", () => {
+		expect(
+			menuReducer(
+				{
+					...initialState,
+					persistent: true,
+					persistentText: "Click on a valid space to auto-fill walls."
+				},
+				{
+					type: EXTEND_WALL
+				}
+			)
+		).toEqual({
+			className: null,
+			persistent: true,
+			persistentText: "Click on a valid space to auto-fill walls.",
 			prevSelectedType: null,
 			temporary: false,
 			temporaryKey: null,
